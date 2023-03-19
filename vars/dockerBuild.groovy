ee658ae7d8b86563124240b364d91e670702b150
Azhar Shaik <127967467+zohera27@users.noreply.github.com>
2023-03-19 08:21:56 +0530

    Update dockerBuild.groovy
    password change

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -3,11 +3,13 @@ def call(String project, String hubUser) {
     bat "docker tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}"
     bat "docker tag ${hubUser}/${project} ${hubUser}/${project}:latest"
     withCredentials([usernamePassword(
-            credentialsId: "docker_cred",
-            usernameVariable: "USER",
-            passwordVariable: "PASS"
+            credentialsId: 'docker_cred',
+            usernameVariable: 'USER',
+            passwordVariable: 'PASS'
     )]) {
-        bat "docker login -u '$USER' --password-stdin '$PASS'"
+        sh """
+            docker login --username="${USER}" --password="${PASS}"
+        """
     }
     bat "docker image push ${hubUser}/${project}:${ImageTag}"
     bat "docker image push ${hubUser}/${project}:latest"
