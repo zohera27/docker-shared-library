74e2d174f9ff3e301aa3de9c719e489e1a1485f3
Azhar Shaik <127967467+zohera27@users.noreply.github.com>
2023-03-19 08:50:46 +0530

    Update dockerBuild.groovy
    gr

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,7 +1,7 @@
 def call(String project, String hubUser) {
-    sh "docker image build -t ${hubUser}/${project} ."
-    sh "docker tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}"
-    sh "docker tag ${hubUser}/${project} ${hubUser}/${project}:latest"
+    bat "docker image build -t ${hubUser}/${project} ."
+    bat "docker tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}"
+    bat "docker tag ${hubUser}/${project} ${hubUser}/${project}:latest"
     environment {
     DOCKERHUB_CREDENTIALS = credentials('docker_cred')
     }
@@ -12,6 +12,6 @@ def call(String project, String hubUser) {
     )]) {
         sh 'echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin'
     }
-    sh "docker image push ${hubUser}/${project}:${ImageTag}"
-    sh "docker image push ${hubUser}/${project}:latest"
+    bat "docker image push ${hubUser}/${project}:${ImageTag}"
+    bat "docker image push ${hubUser}/${project}:latest"
 }
