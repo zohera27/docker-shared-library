2f1de270041a6ded2b399a6afb952d9dbea8f57d
Azhar Shaik <127967467+zohera27@users.noreply.github.com>
2023-03-19 16:03:47 +0530

    Update dockerBuild.groovy
    bat

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -7,9 +7,7 @@ def call(String project, String hubUser) {
             usernameVariable: "USER",
             passwordVariable: "PASS"
     )]) {
-         sh('''docker login --username='$USER' --password='$PASS'
-         .
-         ''')
+        bat "docker login -u '$USER' -p '$PASS'"
     }
     bat "docker image push ${hubUser}/${project}:${ImageTag}"
     bat "docker image push ${hubUser}/${project}:latest"
