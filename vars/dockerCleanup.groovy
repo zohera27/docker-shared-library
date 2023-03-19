0afe5415d9c9bb2f6bbea10ea94b2f793a1c304c
Azhar Shaik <127967467+zohera27@users.noreply.github.com>
2023-03-19 06:23:33 +0530

    Update dockerCleanup.groovy

--- vars/dockerCleanup.groovy
+++ vars/dockerCleanup.groovy
@@ -1,4 +1,4 @@
 def call(String project, String hubUser) {
-    sh "docker rmi ${hubUser}/${project}:${ImageTag}"
-    sh "docker rmi ${hubUser}/${project}:latest"
+    bat "docker rmi ${hubUser}/${project}:${ImageTag}"
+    bat "docker rmi ${hubUser}/${project}:latest"
 }
